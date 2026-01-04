f3ff43617a911bd513b9639189beb04d810964ed
Cleiton Sanches <cleiton.sanches@exemplo.com>
2026-01-04 11:08:00 -0300

    fix: adiciona AccountPayableHistory e User ao AccountsPayableModule para resolver dependÃªncia faltante

--- apps/api/src/modules/accounts-payable/accounts-payable.module.ts
+++ apps/api/src/modules/accounts-payable/accounts-payable.module.ts
@@ -5,9 +5,11 @@ import { AccountsPayableService } from './accounts-payable.service';
 import { AccountPayable } from '../../database/entities/account-payable.entity';
 import { InvoiceAccountPayable } from '../../database/entities/invoice-account-payable.entity';
 import { Invoice } from '../../database/entities/invoice.entity';
+import { AccountPayableHistory } from '../../database/entities/account-payable-history.entity';
+import { User } from '../../database/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([AccountPayable, InvoiceAccountPayable, Invoice])],
+  imports: [TypeOrmModule.forFeature([AccountPayable, InvoiceAccountPayable, Invoice, AccountPayableHistory, User])],
   controllers: [AccountsPayableController],
   providers: [AccountsPayableService],
   exports: [AccountsPayableService],
